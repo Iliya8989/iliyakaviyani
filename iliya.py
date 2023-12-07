363b0fbc6c6de6da10343b8121746f4ed883377a
iliyakaviyani <iliyakaviyani313@gmail.com>
2023-12-07 10:33:35 +0330

    library pytime

---
+++ iliya.py
@@ -0,0 +1,13 @@
+y =int(input("Enter Year: "))
+
+m =int(input("Enter Month: "))
+
+
+def year_month():
+
+    if m>10:
+        my =y+622
+        print(my)
+    else:
+        c =y+621
+        print(c)
